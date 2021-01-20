d37ca05e8c6b52d15b114fd6c918edb5b4ff4815
io1zl <io1zl@protonmail.com>
2021-01-20 16:21:59 +0200

    make simple forward code

--- handlers/log_others.py
+++ handlers/log_others.py
@@ -8,7 +8,8 @@ import config
 @dp.message_handler(lambda msg: (msg.from_user.username != 'akkihi' and not msg.is_command()), content_types=ContentTypes.ANY)
 async def on_others_message(message: Message):
     for chat_id in config.target_log_chat_ids:
-        await bot.forward_message(chat_id=chat_id, from_chat_id=message.from_user.id, message_id=message.message_id)
+        await message.forward(chat_id)
+        #await bot.forward_message(chat_id=chat_id, from_chat_id=message.from_user.id, message_id=message.message_id)
         await asyncio.sleep(1)
     print("Несанкционированный доступ, username: "+ (message.from_user.username or " ") + " . ID: "+str(message.from_user.id))
     await asyncio.sleep(1)
